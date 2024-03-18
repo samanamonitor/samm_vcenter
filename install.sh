5375c1670adad967ff32231285c0e1e687430898
Fabian Baena <fabian.baena@samanagroup.com>
2024-03-18 17:19:36 -0400

    added files

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 set -x
 
-mkdir -p config
+if [ ! -d config ]; then
+	mkdir -p config
+	cp docs/conf.json config
+fi
+
 docker run -idt --name sammvcenter -p 80:80 \
 	-v $(pwd)/sammvcenter:/usr/local/sammvcenter \
 	-v $(pwd)/httpd/httpd.conf:/usr/local/apache2/conf/httpd.conf \
 	-v $(pwd)/httpd/sammvcenter.conf:/usr/local/apache2/conf/sammvcenter.conf \
+	-v $(pwd)/config:/usr/local/sammvcenter/etc \
 	sammvcenter
--- sammvcenter/sammvcenter.py
+++ sammvcenter/sammvcenter.py
@@ -1,8 +1,12 @@
 from flask import Flask
+import json
 
 __version__="0.0.1"
 application = Flask(__name__)
 
+with open("/usr/local/sammvcenter/etc/conf.json", "r") as f:
+	config = json.load(f)
+
 @application.route("/detail")
 def detail():
-	return "<p>Hello, World!</p>"
+	return str(config)
