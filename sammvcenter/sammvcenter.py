2fc7f9d65c654bb09df9fdd42bbb2dc7ba1be94c
Fabian Baena <fabian.baena@samanagroup.com>
2024-03-18 17:17:07 -0400

    added files

--- sammvcenter/sammvcenter.py
+++ sammvcenter/sammvcenter.py
@@ -3,6 +3,6 @@ from flask import Flask
 __version__="0.0.1"
 application = Flask(__name__)
 
-@application.route("/vcenterdetail")
-def vcenterdetail():
+@application.route("/detail")
+def detail():
 	return "<p>Hello, World!</p>"
