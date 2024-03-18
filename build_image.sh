165cc86f68ac4bbc24ef7cb7179bed18793bc8dd
Fabian Baena <fabian.baena@samanagroup.com>
2024-03-18 16:21:36 -0400

    added scripts

--- build_image.sh
+++ build_image.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker build -t sammvcenter:latest  --no-cache .
+if [ "$1" == "force" ]; then
+	docker build -t sammvcenter:latest --no-cache .
+else
+	docker build -t sammvcenter:latest .
+fi	
 
