0cf8eb6a49022a6a02a9345acdeff60bc6ffc990
Fabian Baena <fabian.baena@samanagroup.com>
2024-03-18 16:20:37 -0400

    added scripts

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p config
-docker run --name sammvcenter -p 80:80 \
+docker run -idt --name sammvcenter -p 80:80 \
 	-v $(pwd)/sammvcenter:/usr/local/sammvcenter \
 	-v $(pwd)/config:/usr/local/httpd/sammvcenter \
 	sammvcenter
