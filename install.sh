51fe66789788ec1a8d100d466dbccfd69a2ef89b
Fabian Baena <fabian.baena@samanagroup.com>
2024-07-23 17:43:20 -0400

    set script to accept http and https ports as parameters

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 set -x
 
 SAMMVCETER_PATH=$1
+HTTPPORT="${2:-80}"
+HTTPSPORT="${3:-443}"
+
 if [ -z ${SAMMVCETER_PATH} ]; then
 	echo "Usage: $0 <path to sammvcenter>" >&2
 	exit 1
@@ -14,7 +17,7 @@ if [ ! -d ${SAMMVCETER_PATH} ]; then
 	cp docs/conf.json httpd/samm/sammvcenter.conf ${SAMMVCETER_PATH}
 fi
 
-docker run -idt --name sammvcenter -p 80:80 -p 443:443 \
+docker run -idt --name sammvcenter -p ${HTTPPORT}:80 -p ${HTTPSPORT}:443 \
 	-v ${SAMMVCETER_PATH}/conf.json:/etc/sammvcenter/conf.json \
 	-v ${SAMMVCETER_PATH}/sammvcenter.conf:/usr/local/apache2/conf/extra/sammvcenter.conf \
 	-v ${SAMMVCETER_PATH}/htdocs:/usr/local/apache2/htdocs \
