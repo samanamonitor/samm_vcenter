3cdf14eb22550553163e94e3340cd4ce4e2e2ada
Fabian Baena <fabian.baena@samanagroup.com>
2024-07-23 17:37:32 -0400

    modify install script to bind htdocs folder as well as config files

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 set -x
 
-if [ ! -d config ]; then
-	mkdir -p config
-	cp docs/conf.json config
+SAMMVCETER_PATH=$1
+
+if [ ! -d ${SAMMVCETER_PATH} ]; then
+	mkdir -p ${SAMMVCETER_PATH}/
+	mkdir -p ${SAMMVCETER_PATH}/htdocs
+	cp docs/conf.json httpd/samm/sammvcenter.conf ${SAMMVCETER_PATH}
 fi
 
 docker run -idt --name sammvcenter -p 80:80 -p 443:443 \
-	-v $(pwd)/sammvcenter:/usr/local/sammvcenter \
-	-v $(pwd)/httpd/httpd.conf:/usr/local/apache2/conf/httpd.conf \
-	-v $(pwd)/httpd/samm:/usr/local/apache2/conf/samm \
-	-v $(pwd)/config:/usr/local/sammvcenter/etc \
+	-v ${SAMMVCETER_PATH}/conf.json:/etc/sammvcenter/conf.json \
+	-v ${SAMMVCETER_PATH}/sammvcenter.conf:/usr/local/apache2/conf/extra/sammvcenter.conf \
+	-v ${SAMMVCETER_PATH}/htdocs:/usr/local/apache2/htdocs \
 	sammvcenter
